82b28a6f855fcb19e768b6db2d136158868832e1
Heidi Duchting <h.duechting@gmx.de>
2020-04-13 17:35:58 +0100

    feat: Addig Tweet component

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -1,16 +1,19 @@
 import { getInitialData} from '../utils/api';
 import {receiveUsers} from './users';
 import { receiveTweets} from './tweets';
-import { setAutherUser} from './authedUsers';
+import { setAuthedUser} from './authedUsers';
+import { showLoading, hideLoading } from 'react-redux-loading'
 
 const AUTHED_ID = 'tylermcginnis';
 
 export function handleInitialData () {
   return (dispatch) => {
+    dispatch(showLoading());
     return getInitialData().then(({users, tweets}) => {
       dispatch(receiveUsers(users));
       dispatch(receiveTweets(tweets));
-      dispatch(setAutherUser(AUTHED_ID));
+      dispatch(setAuthedUser(AUTHED_ID));
+      dispatch(hideLoading());
     })
   }
 }
