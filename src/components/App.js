34d402dfe1a030c9cc68a2a646b4352d51a5f858
Heidi Duchting <h.duechting@gmx.de>
2020-04-13 16:32:51 +0100

    feat: adding Dashboard base

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,26 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { handleInitialData} from '../actions/shared';
+import { handleInitialData } from '../actions/shared';
+import Dashboard from './Dashboard';
 
 class App extends Component {
   componentDidMount () {
-    this.props.dispatch(handleInitialData())
+    this.props.dispatch(handleInitialData());
   }
 
-  render() {
+  render () {
     return (
       <div>
-        Starter Code
+        {this.props.loading === true ? null : <Dashboard/>}
       </div>
-    )
+    );
   }
 }
 
+function mapStateToProps ({authedUser}) {
+  return {
+    loading: authedUser === null
+  };
+}
 
-export default connect()(App)
+export default connect(mapStateToProps)(App);
