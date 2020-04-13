1df4c0caddbe3bd545cf1f39d2a35acb778a8c97
Heidi Duchting <h.duechting@gmx.de>
2020-04-13 17:50:00 +0100

    feat: Added functionality to like/unlike a tweet

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -1,27 +1,37 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
-import { formatTweet, formatDate } from '../utils/helpers';
-import { TiArrowBackOutline } from 'react-icons/ti'
-import { TiHeartOutline } from 'react-icons/ti'
-import { TiHeartFullOutline } from 'react-icons/ti'
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { formatDate, formatTweet } from '../utils/helpers';
+import { TiArrowBackOutline, TiHeartFullOutline, TiHeartOutline } from 'react-icons/ti';
+import { handleToggleTweet } from '../actions/tweets';
 
 class Tweet extends Component {
   handleLike = (e) => {
     e.preventDefault();
+    const {dispatch, tweet, authedUser} = this.props;
+
+    dispatch(handleToggleTweet({
+      id: tweet.id,
+      hasLiked: tweet.hasLiked,
+      authedUser
+
+    }));
+
   };
   toParent = (e, id) => {
     e.preventDefault();
     // todo: Redirect to parent
 
   };
-  render() {
-    const { tweet } = this.props;
+
+  render () {
+    const {tweet} = this.props;
 
     if (tweet === null) {
-      return <p>This Tweet does not exist</p>
+      return <p>This Tweet does not exist</p>;
     } else {
       const {
-        name, avatar, timestamp, text, hasLiked, likes, replies, parent } = tweet;
+        name, avatar, timestamp, text, hasLiked, likes, replies, parent
+      } = tweet;
 
       return (
         <div className='tweet'>
@@ -33,43 +43,43 @@ class Tweet extends Component {
           />
           <div className="tweet-info">
             <div>
-            <span>{name}</span>
-            <div>
-              {formatDate(timestamp)}
+              <span>{name}</span>
+              <div>
+                {formatDate(timestamp)}
+              </div>
+              {parent && (
+                <button className='replying-to' onClick={(e) => this.toParent(e, parent.id)}>
+                  Replying to @{parent.author}
+                </button>
+
+              )}
+              <p>{text}</p>
             </div>
-            {parent && (
-              <button className='replying-to' onClick={(e) => this.toParent(e, parent.id)}>
-                Replying to @{parent.author}
+
+            <div className='tweet-icons'>
+              <TiArrowBackOutline className='tweet-icon'/>
+              <span>{replies !== 0 && replies}</span>
+              <button className='heart-button' onClick={this.handleLike}>
+                {hasLiked === true ? <TiHeartFullOutline color='#e0245e' className='tweet-icon'/> : <TiHeartOutline
+                  className='tweet-icon'/>}
               </button>
+              <span>{likes !== 0 && likes}</span>
 
-            )}
-            <p>{text}</p>
             </div>
-
-          <div className='tweet-icons'>
-            <TiArrowBackOutline className='tweet-icon'/>
-            <span>{replies !== 0 && replies }</span>
-            <button className='heart-button' onClick={this.handleLike}>
-              {hasLiked === true ? <TiHeartFullOutline color='#e0245e' className='tweet-icon'/> : <TiHeartOutline  className='tweet-icon'/>}
-            </button>
-            <span>{likes !== 0 && likes}</span>
-
-          </div>
           </div>
         </div>
-      )
+      );
     }
   }
 }
 
-
-function mapStateToProps({authedUser, users, tweets}, { id }) {
+function mapStateToProps ({authedUser, users, tweets}, {id}) {
   const tweet = tweets[id];
-  const parentTweet = tweet? tweets[tweet.replyingTo] : null;
+  const parentTweet = tweet ? tweets[tweet.replyingTo] : null;
   return {
     authedUser,
     tweet: tweet ? formatTweet(tweet, users[tweet.author], authedUser, parentTweet) : null
-  }
+  };
 }
 
 export default connect(mapStateToProps)(Tweet);
